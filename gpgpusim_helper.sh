00460ed5f3d2cf2502d0fd044e7b29947ab3031c
Jongho Kim <scurry2119@gmail.com>
2016-04-17 08:21:28 +0900

    GPGPUSim Prerequisite Install Script

---
+++ gpgpusim_helper.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y gcc g++ make build-essential xutils-dev bison zlib1g-dev flex libglu1-mesa-dev libxi-dev libxmu-dev libglut3-dev
+#documentation dependencies:
+#sudo apt-get install doxygen graphviz
