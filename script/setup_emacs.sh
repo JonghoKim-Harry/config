e9b4d26e3bc107bf0a5dce0f09105a20af086108
Jongho Kim <scurry2119@gmail.com>
2020-10-12 15:11:33 +0900

    Force to install latest emacs

--- script/setup_emacs.sh
+++ script/setup_emacs.sh
@@ -5,3 +5,7 @@ ROOT=$(realpath $(dirname $0)/..)
 # Copy config files
 cp $ROOT/.emacs $HOME
 cp -r $ROOT/.emacs.d $HOME
+
+# Install the latest emacs
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get install emacs
